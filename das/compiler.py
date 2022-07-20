5b97626c58f326385f0ae64bdc5bdbf1fd85eb04
David Sanders <davesque@gmail.com>
2022-07-20 14:26:57 -0700

    Add compiler and tests

---
+++ das/compiler.py
@@ -0,0 +1,73 @@
+from typing import Iterator
+
+from .exceptions import RenderedError
+from .parser import File, Label, Op, Val
+
+
+class Compiler:
+    OPS = {
+        "nop": (0b0000, 0),
+        "lda": (0b0001, 1),
+        "add": (0b0010, 1),
+        "sub": (0b0011, 1),
+        "sta": (0b0100, 1),
+        "ldi": (0b0101, 1),
+        "jmp": (0b0110, 1),
+        "jc": (0b0111, 1),
+        "jz": (0b1000, 1),
+        "out": (0b1110, 0),
+        "hlt": (0b1111, 0),
+    }
+
+    def __init__(self, file: File):
+        self.file = file
+        self.labels = {}
+
+        self.find_labels()
+
+    def find_labels(self) -> None:
+        loc = 0
+        for stmt in self.file.stmts:
+            if loc > 15:
+                raise RenderedError(f"statement makes program too large", stmt.toks[0])
+
+            if isinstance(stmt, Label):
+                if stmt.name in self.labels:
+                    raise RenderedError(f"redefinition of label '{stmt.name}'", stmt.toks[0])
+
+                self.labels[stmt.name] = loc
+            else:
+                loc += 1
+
+    def __iter__(self) -> Iterator[int]:
+        for stmt in self.file.stmts:
+            if isinstance(stmt, Op):
+                code, arity = self.OPS[stmt.mnemonic]
+
+                if arity == 1 and stmt.arg is None:
+                    raise RenderedError(f"operation '{stmt.mnemonic}' requires an argument", stmt.toks[0])
+
+                if arity == 0 and stmt.arg is not None:
+                    raise RenderedError(f"operation '{stmt.mnemonic}' does not take an argument", stmt.toks[0])
+
+                if isinstance(stmt.arg, str):
+                    if stmt.arg not in self.labels:
+                        raise RenderedError(f"unrecognized label '{stmt.arg}'", stmt.toks[1])
+
+                    loc = self.labels[stmt.arg]
+                    yield (code << 4) + loc
+                elif isinstance(stmt.arg, int):
+                    if stmt.arg > 15:
+                        raise RenderedError(f"arg '{stmt.arg}' is too large", stmt.toks[1])
+
+                    yield (code << 4) + stmt.arg
+                elif stmt.arg is None:
+                    yield code << 4
+                else:
+                    raise Exception("invariant")
+
+            elif isinstance(stmt, Val):
+                if stmt.val > 255:
+                    raise RenderedError(f"value '{stmt.toks[0].text}' is too large", stmt.toks[0])
+
+                yield stmt.val
---
+++ tests/test_compiler.py
@@ -0,0 +1,65 @@
+from io import StringIO
+
+from das.lexer import Lexer
+from das.parser import Parser
+from das.compiler import Compiler
+
+
+def test_compile_count() -> None:
+    buf = StringIO("""
+# Counts from 42 to 256 (zero really in 8 bits), then down from 255 to 1
+# before halting
+
+lda init
+
+count_up:
+  out
+  add incr
+  jc count_down  # jump to "count_down" if we overflowed
+  jmp count_up
+
+count_down:
+  out
+  sub incr
+  jz end         # jump to "end" if we hit zero
+  jmp count_down
+
+end: hlt
+
+init: 42
+incr: 1
+""")
+    lexer = Lexer(buf)
+    parser = Parser(lexer)
+    file = parser.parse_file()
+    compiler = Compiler(file)
+
+    assert list(compiler) == [26, 224, 43, 117, 97, 224, 59, 137, 101, 240, 42, 1]
+
+
+def test_compile_fib() -> None:
+    buf = StringIO("""
+# Counts up in fibonacci numbers forever (with a lot of overflow)
+
+loop:
+  lda a
+  out
+  add b
+  sta a
+
+  lda b
+  out
+  add a
+  sta b
+
+  jmp loop
+
+a: 1
+b: 1
+""")
+    lexer = Lexer(buf)
+    parser = Parser(lexer)
+    file = parser.parse_file()
+    compiler = Compiler(file)
+
+    assert list(compiler) == [25, 224, 42, 73, 26, 224, 41, 74, 96, 1, 1]
