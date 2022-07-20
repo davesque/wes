90b3d562c7e792f0e6ea847b352689f248e62883
David Sanders <davesque@gmail.com>
2022-07-20 14:26:58 -0700

    Close out coverage

--- das/cli.py
+++ das/cli.py
@@ -7,41 +7,32 @@ from .lexer import Lexer
 from .parser import Parser
 
 
-def run(f: TextIO, out: TextIO) -> None:
-    lexer = Lexer(f)
+def run(in_buf: TextIO, out_buf: TextIO) -> None:
+    lexer = Lexer(in_buf)
     parser = Parser(lexer)
 
+    file = parser.parse_file()
+    compiler = Compiler(file)
+
+    for i, code in enumerate(compiler):
+        print(f"{i:04b}: {code >> 4:04b} {code & 15:04b}", file=out_buf)
+
+
+def main():  # pragma: no cover
     try:
-        file = parser.parse_file()
+        if len(sys.argv) == 1:
+            run(sys.stdin, sys.stdout)
+        else:
+            if sys.argv[1] == "-h":
+                sys.stderr.write("usage: das <filename> | das < <filename>\n")
+                sys.exit(1)
+
+            with open(sys.argv[1], "r") as f:
+                run(f, sys.stdout)
     except RenderedError as e:
         print(e.render(), file=sys.stderr)
         sys.exit(1)
 
-    try:
-        compiler = Compiler(file)
-    except RenderedError as e:
-        print(e.render(), file=sys.stderr)
-        sys.exit(1)
 
-    try:
-        for i, code in enumerate(compiler):
-            print(f"{i:04b}: {code >> 4:04b} {code & 15:04b}", file=out)
-    except RenderedError as e:
-        print(e.render(), file=sys.stderr)
-        sys.exit(1)
-
-
-def main():
-    if len(sys.argv) == 1:
-        run(sys.stdin, sys.stdout)
-    else:
-        if sys.argv[1] == "-h":
-            sys.stderr.write("usage: das <filename> | das < <filename>\n")
-            sys.exit(1)
-
-        with open(sys.argv[1], "r") as f:
-            run(f, sys.stdout)
-
-
-if __name__ == "__main__":
+if __name__ == "__main__":  # pragma: no cover
     main()
---
+++ tests/test_cli.py
@@ -0,0 +1,52 @@
+from io import StringIO
+
+from das.cli import run
+
+
+def test_run() -> None:
+    in_buf = StringIO(
+        """
+# Counts from 42 to 256 (zero really in 8 bits), then down from 255 to 1
+# before halting
+
+lda init
+
+count_up:
+  out
+  add incr
+  jc count_down  # jump to "count_down" if we overflowed
+  jmp count_up
+
+count_down:
+  out
+  sub incr
+  jz end         # jump to "end" if we hit zero
+  jmp count_down
+
+end: hlt
+
+init: 42
+incr: 1
+"""
+    )
+    out_buf = StringIO()
+
+    run(in_buf, out_buf)
+    out_buf.seek(0)
+
+    # fmt: off
+    assert out_buf.read() == """
+0000: 0001 1010
+0001: 1110 0000
+0010: 0010 1011
+0011: 0111 0101
+0100: 0110 0001
+0101: 1110 0000
+0110: 0011 1011
+0111: 1000 1001
+1000: 0110 0101
+1001: 1111 0000
+1010: 0010 1010
+1011: 0000 0001
+"""[1:]
+    # fmt: on
