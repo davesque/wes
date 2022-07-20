c4810629905c4965371a0a57b03e8ebd2ea6104f
David Sanders <davesque@gmail.com>
2022-07-20 14:26:57 -0700

    Add Token method tests

--- tests/test_lexer.py
+++ tests/test_lexer.py
@@ -1,4 +1,4 @@
-from das.lexer import tokenize
+from das.lexer import tokenize, Token
 
 
 def test_tokenize() -> None:
@@ -16,3 +16,19 @@ def test_tokenize() -> None:
         "baz",
         "\n\n",
     ]
+
+
+def test_token_is_newline() -> None:
+    tok = Token("\n", 0, 0, "", 0, 0, 0)
+    assert tok.is_newline
+
+    tok = Token("  \n", 0, 0, "", 0, 0, 0)
+    assert not tok.is_newline
+
+
+def test_token_is_eof() -> None:
+    tok = Token("", 0, 0, "", 0, 0, 0)
+    assert tok.is_eof
+
+    tok = Token("  \n", 0, 0, "", 0, 0, 0)
+    assert not tok.is_eof
