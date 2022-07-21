1f2d7eb5b2a224ca1478f5b962d8e7f2ca70ded8
David Sanders <davesque@gmail.com>
2022-07-21 13:18:42 -0700

    Require file text for error rendering

--- das/exceptions.py
+++ das/exceptions.py
@@ -14,13 +14,22 @@ class RenderedError(ParseError):
         self.msg = msg
         self.tok = tok
 
-    def render(self) -> str:
+    @staticmethod
+    def _get_line(file_txt: str, line_start: int) -> str:
+        line_end = file_txt.find("\n", line_start)
+        if line_end == -1:
+            line_end = len(file_txt)
+
+        return file_txt[line_start:line_end]
+
+    def render(self, file_txt: str) -> str:
+        line = self._get_line(file_txt, self.tok.line_start)
         marker_str = " " * self.tok.col + "^" * len(self.tok.text)
 
         # fmt:off
         return f"""
 at line {self.tok.line_num}, col {self.tok.col}:
-{self.tok.line.rstrip()}
+{line}
 {marker_str}
 
 {self.msg}
--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -5,12 +5,27 @@ from das.lexer import Lexer
 
 
 def test_rendered_error_render() -> None:
-    toks = list(Lexer(StringIO("the quick brown fox jumped over the lazy dogs")))
-    e = RenderedError("problem here", toks[3])
+    file_txt = "line one\nthe quick brown fox jumped over the lazy dogs"
+    toks = list(Lexer(StringIO(file_txt)))
+    e = RenderedError("problem here", toks[6])
 
     # fmt: off
-    assert e.render() == """
-at line 1, col 16:
+    assert e.render(file_txt) == """
+at line 2, col 16:
+the quick brown fox jumped over the lazy dogs
+                ^^^
+
+problem here
+"""[1:-1]
+    # fmt: on
+
+    file_txt = "line one\nthe quick brown fox jumped over the lazy dogs\n"
+    toks = list(Lexer(StringIO(file_txt)))
+    e = RenderedError("problem here", toks[6])
+
+    # fmt: off
+    assert e.render(file_txt) == """
+at line 2, col 16:
 the quick brown fox jumped over the lazy dogs
                 ^^^
 
