a7323cf408282b699ab8098172df960546b2f372
David Sanders <davesque@gmail.com>
2022-07-22 16:34:12 -0700

    Simplify compiler tests

--- tests/test_compiler.py
+++ tests/test_compiler.py
@@ -4,12 +4,10 @@ import pytest
 
 from das.compiler import Compiler
 from das.exceptions import RenderedError
-from das.lexer import Lexer
-from das.parser import Parser
 
 
 def test_compile_count() -> None:
-    buf = StringIO(
+    compiler = Compiler.from_str(
         """
 ; Counts from 42 to 256 (zero really in 8 bits), then down from 255 to 1
 ; before halting
@@ -34,16 +32,11 @@ init: 42
 incr: 1
 """
     )
-    lexer = Lexer(buf)
-    parser = Parser(lexer)
-    file = parser.parse_file()
-    compiler = Compiler(file)
-
     assert list(compiler) == [26, 224, 43, 117, 97, 224, 59, 137, 101, 240, 42, 1]
 
 
 def test_compile_fib() -> None:
-    buf = StringIO(
+    compiler = Compiler.from_str(
         """
 ; Counts up in fibonacci numbers forever (with a lot of overflow)
 
@@ -64,30 +57,16 @@ a: 1
 b: 1
 """
     )
-    lexer = Lexer(buf)
-    parser = Parser(lexer)
-    file = parser.parse_file()
-    compiler = Compiler(file)
-
     assert list(compiler) == [25, 224, 42, 73, 26, 224, 41, 74, 96, 1, 1]
 
 
 def test_compile_op_with_literal() -> None:
-    buf = StringIO(
-        """
-lda 1
-"""
-    )
-    lexer = Lexer(buf)
-    parser = Parser(lexer)
-    file = parser.parse_file()
-    compiler = Compiler(file)
-
+    compiler = Compiler.from_str("lda 1")
     assert list(compiler) == [17]
 
 
 def test_compile_too_large() -> None:
-    buf = StringIO(
+    compiler = Compiler.from_str(
         """
 0
 0
@@ -108,109 +87,61 @@ def test_compile_too_large() -> None:
 too_big
 """
     )
-    lexer = Lexer(buf)
-    parser = Parser(lexer)
-    file = parser.parse_file()
-
     with pytest.raises(RenderedError) as excinfo:
-        list(Compiler(file))
+        list(compiler)
 
     assert "statement makes program too large" in excinfo.value.msg
 
 
 def test_compile_duplicate_label() -> None:
-    buf = StringIO(
+    compiler = Compiler.from_str(
         """
 foo: 1
 foo: 1
 """
     )
-    lexer = Lexer(buf)
-    parser = Parser(lexer)
-    file = parser.parse_file()
-
     with pytest.raises(RenderedError) as excinfo:
-        list(Compiler(file))
+        list(compiler)
 
     assert "redefinition of label" in excinfo.value.msg
 
 
 def test_compile_bad_unary_op() -> None:
-    buf = StringIO(
-        """
-lda
-"""
-    )
-    lexer = Lexer(buf)
-    parser = Parser(lexer)
-    file = parser.parse_file()
-
+    compiler = Compiler.from_str("lda")
     with pytest.raises(RenderedError) as excinfo:
-        list(Compiler(file))
+        list(compiler)
 
     assert "requires an argument" in excinfo.value.msg
 
 
 def test_compile_bad_nullary_op() -> None:
-    buf = StringIO(
-        """
-nop 1
-"""
-    )
-    lexer = Lexer(buf)
-    parser = Parser(lexer)
-    file = parser.parse_file()
-
+    compiler = Compiler.from_str("nop 1")
     with pytest.raises(RenderedError) as excinfo:
-        list(Compiler(file))
+        list(compiler)
 
     assert "takes no argument" in excinfo.value.msg
 
 
 def test_compile_bad_label() -> None:
-    buf = StringIO(
-        """
-lda foo
-"""
-    )
-    lexer = Lexer(buf)
-    parser = Parser(lexer)
-    file = parser.parse_file()
-
+    compiler = Compiler.from_str("lda foo")
     with pytest.raises(RenderedError) as excinfo:
-        list(Compiler(file))
+        list(compiler)
 
     assert "unrecognized label" in excinfo.value.msg
 
 
 def test_compile_big_op_arg() -> None:
-    buf = StringIO(
-        """
-lda 16
-"""
-    )
-    lexer = Lexer(buf)
-    parser = Parser(lexer)
-    file = parser.parse_file()
-
+    compiler = Compiler.from_str("lda 16")
     with pytest.raises(RenderedError) as excinfo:
-        list(Compiler(file))
+        list(compiler)
 
     assert "is too large" in excinfo.value.msg
 
 
 def test_compile_big_bare_literal() -> None:
-    buf = StringIO(
-        """
-256
-"""
-    )
-    lexer = Lexer(buf)
-    parser = Parser(lexer)
-    file = parser.parse_file()
-
+    compiler = Compiler.from_str("256")
     with pytest.raises(RenderedError) as excinfo:
-        list(Compiler(file))
+        list(compiler)
 
     assert "is too large" in excinfo.value.msg
 
