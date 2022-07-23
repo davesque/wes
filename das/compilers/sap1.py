6eb58e368282338c0510da1378648e1986d2dd3c
David Sanders <davesque@gmail.com>
2022-07-23 00:56:17 -0700

    Add Instruction classes

--- das/compilers/sap1.py
+++ das/compilers/sap1.py
@@ -5,22 +5,143 @@ from das.exceptions import RenderedError
 from das.parser import Op, Val
 
 
+class Instruction:
+    mnemonic: str = None  # type: ignore
+
+    __slots__ = ("compiler", "op")
+
+    compiler: Compiler
+    op: Op
+
+    def __init__(self, compiler: Compiler, op: Op):
+        self.compiler = compiler
+        self.op = op
+
+        self.validate()
+
+    def validate(self) -> None:
+        raise NotImplementedError("must define `validate`")
+
+    def __iter__(self) -> Iterator[int]:
+        raise NotImplementedError("must define `__iter__`")
+
+
+class Nullary(Instruction):
+    def validate(self) -> None:
+        if self.op.arg is not None:
+            raise RenderedError(
+                f"'{self.mnemonic}' instruction takes no argument",
+                self.op.toks,
+            )
+
+
+class Unary(Instruction):
+    def validate(self) -> None:
+        if self.op.arg is None:
+            raise RenderedError(
+                f"'{self.mnemonic}' instruction requires an argument",
+                self.op.toks,
+            )
+
+
+class SapUnary(Unary):
+    code: int = None  # type: ignore
+
+    def __iter__(self) -> Iterator[int]:
+        if isinstance(self.op.arg, str):
+            loc = self.compiler.resolve_label(self.op.arg, self.op.toks[1])
+            yield (self.code << 4) + loc
+
+        elif isinstance(self.op.arg, int):
+            if self.op.arg > self.compiler.max_addr:
+                raise RenderedError(
+                    f"arg '{self.op.arg}' is too large", self.op.toks[1]
+                )
+
+            yield (self.code << 4) + self.op.arg
+
+        else:
+            raise Exception("invariant")
+
+
+class Const(Nullary):
+    output: int = None  # type: ignore
+
+    def __iter__(self) -> Iterator[int]:
+        yield self.output
+
+
+class Nop(Const):
+    mnemonic = "nop"
+    output = 0b00000000
+
+
+class Out(Const):
+    mnemonic = "out"
+    output = 0b11100000
+
+
+class Hlt(Const):
+    mnemonic = "hlt"
+    output = 0b11110000
+
+
+class Lda(SapUnary):
+    mnemonic = "lda"
+    code = 0b0001
+
+
+class Add(SapUnary):
+    mnemonic = "add"
+    code = 0b0010
+
+
+class Sub(SapUnary):
+    mnemonic = "sub"
+    code = 0b0011
+
+
+class Sta(SapUnary):
+    mnemonic = "sta"
+    code = 0b0100
+
+
+class Ldi(SapUnary):
+    mnemonic = "ldi"
+    code = 0b0101
+
+
+class Jmp(SapUnary):
+    mnemonic = "jmp"
+    code = 0b0110
+
+
+class Jc(SapUnary):
+    mnemonic = "jc"
+    code = 0b0111
+
+
+class Jz(SapUnary):
+    mnemonic = "jz"
+    code = 0b1000
+
+
 class Sap1(Compiler):
     max_addr = 15
     max_val = 255
 
-    ops = {
-        "nop": (0b0000, 0),
-        "lda": (0b0001, 1),
-        "add": (0b0010, 1),
-        "sub": (0b0011, 1),
-        "sta": (0b0100, 1),
-        "ldi": (0b0101, 1),
-        "jmp": (0b0110, 1),
-        "jc": (0b0111, 1),
-        "jz": (0b1000, 1),
-        "out": (0b1110, 0),
-        "hlt": (0b1111, 0),
+    instructions = {
+        Nop.mnemonic: Nop,
+        Lda.mnemonic: Lda,
+        Add.mnemonic: Add,
+        Sub.mnemonic: Sub,
+        Sta.mnemonic: Sta,
+        Ldi.mnemonic: Ldi,
+        Jmp.mnemonic: Jmp,
+        Jc.mnemonic: Jc,
+        Jz.mnemonic: Jz,
+        Out.mnemonic: Out,
+        Hlt.mnemonic: Hlt,
     }
 
     def __iter__(self) -> Iterator[int]:
@@ -28,34 +149,15 @@ class Sap1(Compiler):
 
         for stmt in self.file.stmts:
             if isinstance(stmt, Op):
-                code, arity = self.ops[stmt.mnemonic]
-
-                if arity == 1 and stmt.arg is None:
+                try:
+                    instruction_cls = self.instructions[stmt.mnemonic]
+                except KeyError:
                     raise RenderedError(
-                        f"operation '{stmt.mnemonic}' requires an argument",
-                        stmt.toks[0],
+                        f"unrecognized instruction '{stmt.mnemonic}'", stmt.toks
                     )
 
-                if arity == 0 and stmt.arg is not None:
-                    raise RenderedError(
-                        f"operation '{stmt.mnemonic}' takes no argument",
-                        stmt.toks[0],
-                    )
-
-                if isinstance(stmt.arg, str):
-                    loc = self.resolve_label(stmt.arg, stmt.toks[1])
-                    yield (code << 4) + loc
-                elif isinstance(stmt.arg, int):
-                    if stmt.arg > self.max_addr:
-                        raise RenderedError(
-                            f"arg '{stmt.arg}' is too large", stmt.toks[1]
-                        )
-
-                    yield (code << 4) + stmt.arg
-                elif stmt.arg is None:
-                    yield code << 4
-                else:  # pragma: no cover
-                    raise Exception("invariant")
+                inst = instruction_cls(self, stmt)
+                yield from inst
 
             elif isinstance(stmt, Val):
                 if stmt.val > self.max_val:
