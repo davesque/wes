35ef4ad0c873fb3ff1579cf38ce7c74aa9b9846d
David Sanders <davesque@gmail.com>
2022-07-22 22:04:13 -0700

    Add support for token spans in error messages

--- das/exceptions.py
+++ das/exceptions.py
@@ -1,3 +1,5 @@
+from typing import List, Union
+
 from .lexer import Eof, Newline, Text, Token
 
 
@@ -10,9 +12,19 @@ class EndOfTokens(ParseError):
 
 
 class RenderedError(ParseError):
-    def __init__(self, msg: str, tok: Token):
+    def __init__(self, msg: str, toks: Union[Token, List[Token]]):
+        if isinstance(toks, Token):
+            toks = [toks]
+
+        if len(toks) == 0:
+            raise ValueError("expected one or more tokens")
+
+        line_num = toks[0].line_num
+        if any(t.line_num != line_num for t in toks):
+            raise ValueError("tokens not from same line")
+
         self.msg = msg
-        self.tok = tok
+        self.toks = toks
 
     @staticmethod
     def _get_line(file_txt: str, line_start: int) -> str:
@@ -25,27 +37,33 @@ class RenderedError(ParseError):
         return file_txt[line_start:line_end]
 
     def render(self, file_txt: str) -> str:
-        line = self._get_line(file_txt, self.tok.line_start)
+        fst, lst = self.toks[0], self.toks[-1]
+        line = self._get_line(file_txt, fst.line_start).strip()
 
-        if isinstance(self.tok, Text):
-            marker = "^" * len(self.tok.text)
-        elif isinstance(self.tok, (Newline, Eof)):
-            marker = "^"
-        else:  # pragma: no cover
-            raise Exception("invariant")
-        marker_str = " " * self.tok.col + marker
-
-        if line.endswith("\n") and isinstance(self.tok, Eof):
-            line = line.strip() + r"\n"
-            marker_str = " " + marker_str
+        marker_start = min(len(line), fst.col)
+        if len(self.toks) == 1:
+            if isinstance(fst, Text):
+                marker_len = len(fst.text)
+            elif isinstance(fst, (Newline, Eof)):
+                marker_len = 1
+            else:  # pragma: no cover
+                raise Exception("invariant")
         else:
-            line = line.strip()
+            if isinstance(lst, Text):
+                marker_end = lst.col + len(lst.text)
+            elif isinstance(lst, (Newline, Eof)):
+                marker_end = min(len(line), lst.col)
+            else:  # pragma: no cover
+                raise Exception("invariant")
+            marker_len = max(1, marker_end - marker_start)
+
+        marker_string = " " * marker_start + "^" * marker_len
 
         # fmt:off
         return f"""
-at line {self.tok.line_num}, col {self.tok.col}:
+at line {fst.line_num}, col {fst.col}:
 {line}
-{marker_str}
+{marker_string}
 
 {self.msg}
 """[1:-1]
--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -1,4 +1,4 @@
-from io import StringIO
+import pytest
 
 from das.exceptions import RenderedError
 from das.lexer import Lexer
@@ -6,7 +6,7 @@ from das.lexer import Lexer
 
 def test_rendered_error_render_text() -> None:
     file_txt = "line one\nthe quick brown fox jumped over the lazy dogs"
-    toks = list(Lexer(StringIO(file_txt)))
+    toks = list(Lexer.from_str(file_txt))
     e = RenderedError("problem here", toks[6])
 
     # fmt: off
@@ -20,7 +20,7 @@ problem here
     # fmt: on
 
     file_txt = "line one\nthe quick brown fox jumped over the lazy dogs\n"
-    toks = list(Lexer(StringIO(file_txt)))
+    toks = list(Lexer.from_str(file_txt))
     e = RenderedError("problem here", toks[6])
 
     # fmt: off
@@ -36,7 +36,23 @@ problem here
 
 def test_rendered_error_render_newline() -> None:
     file_txt = "test line"
-    toks = list(Lexer(StringIO(file_txt)))
+    toks = list(Lexer.from_str(file_txt))
+    e = RenderedError("expected newline", toks[2])
+
+    # fmt: off
+    assert e.render(file_txt) == """
+at line 1, col 9:
+test line
+         ^
+
+expected newline
+"""[1:-1]
+    # fmt: on
+
+
+def test_rendered_error_render_newline_with_nl() -> None:
+    file_txt = "test line\n"
+    toks = list(Lexer.from_str(file_txt))
     e = RenderedError("expected newline", toks[2])
 
     # fmt: off
@@ -52,7 +68,7 @@ expected newline
 
 def test_rendered_error_render_eof() -> None:
     file_txt = "test line"
-    toks = list(Lexer(StringIO(file_txt)))
+    toks = list(Lexer.from_str(file_txt))
     e = RenderedError("expected eof", toks[3])
 
     # fmt: off
@@ -68,15 +84,89 @@ expected eof
 
 def test_rendered_error_render_eof_with_nl() -> None:
     file_txt = "test line\n"
-    toks = list(Lexer(StringIO(file_txt)))
+    toks = list(Lexer.from_str(file_txt))
     e = RenderedError("expected eof", toks[3])
 
     # fmt: off
     assert e.render(file_txt) == r"""
 at line 1, col 10:
-test line\n
-           ^
+test line
+         ^
 
 expected eof
 """[1:-1]
     # fmt: on
+
+
+def test_rendered_error_multiple_tokens() -> None:
+    file_txt = "line one\nthe quick brown fox jumped over the lazy dogs"
+    toks = list(Lexer.from_str(file_txt))
+
+    e = RenderedError("problem here", toks[5:8])
+    # fmt: off
+    assert e.render(file_txt) == """
+at line 2, col 10:
+the quick brown fox jumped over the lazy dogs
+          ^^^^^^^^^^^^^^^^
+
+problem here
+"""[1:-1]
+    # fmt: on
+
+    e = RenderedError("problem here", [toks[5], toks[7]])
+    # fmt: off
+    assert e.render(file_txt) == """
+at line 2, col 10:
+the quick brown fox jumped over the lazy dogs
+          ^^^^^^^^^^^^^^^^
+
+problem here
+"""[1:-1]
+    # fmt: on
+
+    e = RenderedError("problem here", toks[1:3])
+    # fmt: off
+    assert e.render(file_txt) == """
+at line 1, col 5:
+line one
+     ^^^
+
+problem here
+"""[1:-1]
+    # fmt: on
+
+    file_txt = "line one\n"
+    toks = list(Lexer.from_str(file_txt))
+
+    e = RenderedError("problem here", toks[1:])
+    # fmt: off
+    assert e.render(file_txt) == """
+at line 1, col 5:
+line one
+     ^^^
+
+problem here
+"""[1:-1]
+    # fmt: on
+
+    e = RenderedError("problem here", toks[2:])
+    # fmt: off
+    assert e.render(file_txt) == """
+at line 1, col 8:
+line one
+        ^
+
+problem here
+"""[1:-1]
+    # fmt: on
+
+
+def test_rendered_error_value_errors() -> None:
+    with pytest.raises(ValueError, match="expected one or more tokens"):
+        RenderedError("foo", [])
+
+    file_txt = "line one\nthe quick brown fox jumped over the lazy dogs"
+    toks = list(Lexer.from_str(file_txt))
+
+    with pytest.raises(ValueError, match="tokens not from same line"):
+        RenderedError("foo", toks[0:5])
