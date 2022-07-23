a6fed1d8dc10307a1920c1ad51fb74e09fd523a8
David Sanders <davesque@gmail.com>
2022-07-23 01:33:24 -0700

    Reorder some tests

--- tests/test_compiler.py
+++ tests/test_compiler.py
@@ -7,6 +7,14 @@ from das.compilers.sap1 import Sap1
 from das.exceptions import RenderedError
 
 
+def test_compiler_from_str() -> None:
+    assert list(Sap1.from_str("255")) == [255]
+
+
+def test_compiler_from_buf() -> None:
+    assert list(Sap1.from_buf(StringIO("255"))) == [255]
+
+
 def test_compiler_find_labels() -> None:
     compiler = Compiler.from_str(
         """
@@ -22,25 +30,6 @@ two: 0x44
     assert compiler.labels["zero"] == 0
     assert compiler.labels["one"] == 1
     assert compiler.labels["two"] == 2
-
-
-def test_compiler_resolve_label() -> None:
-    compiler = Compiler.from_str(
-        """
-lda meaning_of_life
-forty_two: 0x42
-"""
-    )
-    compiler.find_labels()
-
-    bad_label_tok = compiler.file.stmts[0].toks[1]
-    with pytest.raises(RenderedError) as excinfo:
-        compiler.resolve_label("meaning_of_life", bad_label_tok)
-    assert excinfo.value.msg == "unrecognized label 'meaning_of_life'"
-    assert excinfo.value.toks == (bad_label_tok,)
-
-    good_label_tok = compiler.file.stmts[1].toks[1]
-    assert compiler.resolve_label("forty_two", good_label_tok) == 1
 
 
 def test_find_labels_too_large() -> None:
@@ -84,9 +73,20 @@ foo: 1
     assert "redefinition of label" in excinfo.value.msg
 
 
-def test_compiler_from_str() -> None:
-    assert list(Sap1.from_str("255")) == [255]
+def test_compiler_resolve_label() -> None:
+    compiler = Compiler.from_str(
+        """
+lda meaning_of_life
+forty_two: 0x42
+"""
+    )
+    compiler.find_labels()
 
+    bad_label_tok = compiler.file.stmts[0].toks[1]
+    with pytest.raises(RenderedError) as excinfo:
+        compiler.resolve_label("meaning_of_life", bad_label_tok)
+    assert excinfo.value.msg == "unrecognized label 'meaning_of_life'"
+    assert excinfo.value.toks == (bad_label_tok,)
 
-def test_compiler_from_buf() -> None:
-    assert list(Sap1.from_buf(StringIO("255"))) == [255]
+    good_label_tok = compiler.file.stmts[1].toks[1]
+    assert compiler.resolve_label("forty_two", good_label_tok) == 1
