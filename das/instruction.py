49a8023643a51e2f74f9c7aed1c6cae189da8b3c
David Sanders <davesque@gmail.com>
2022-07-23 17:00:17 -0700

    Add instruction.Value

--- das/instruction.py
+++ das/instruction.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 from typing import TYPE_CHECKING, Iterator
 
 from das.exceptions import RenderedError
-from das.parser import Op
+from das.parser import Op, Val
 from das.utils import byte_length
 
 if TYPE_CHECKING:  # pragma: no cover
@@ -39,6 +39,29 @@ class Instruction(BaseInstruction):
     def __init__(self, compiler: Compiler, op: Op):
         self.op = op
         super().__init__(compiler)
+
+
+class Value(BaseInstruction):
+    __slots__ = ("val",)
+
+    val: Val
+
+    def __init__(self, compiler: Compiler, val: Val):
+        self.val = val
+        super().__init__(compiler)
+
+    def validate(self) -> None:
+        if self.val.val > self.compiler.max_val:
+            raise RenderedError(
+                f"value '{self.val.toks[0].text}' is too large", self.val.toks[0]
+            )
+
+    def encode(self) -> Iterator[int]:
+        yield self.val.val
+
+    @property
+    def size(self) -> int:
+        return byte_length(self.val.val)
 
 
 class Nullary(Instruction):
