56c765af1727a710d32b05930fbc499335415ff8
David Sanders <davesque@gmail.com>
2022-07-26 21:34:15 -0700

    Add argparse and formatters

--- wes/cli.py
+++ wes/cli.py
@@ -1,36 +1,90 @@
+import argparse
+import shutil
 import sys
 from io import StringIO
-from typing import TextIO
+from typing import Callable, TextIO
 
+from wes.compiler import Compiler
 from wes.compilers.sap import SapCompiler
 from wes.exceptions import Message, Stop
 
+Formatter = Callable[[Compiler], None]
 
-def run(in_buf: TextIO, out_buf: TextIO) -> None:
+
+def binary_text(compiler: Compiler) -> None:
+    for i, code in enumerate(compiler):
+        print(f"{i:04b}: {code >> 4:04b} {code & 15:04b}", file=sys.stdout)
+
+
+class ReadCompiler:
+    def __init__(self, compiler: Compiler):
+        self.it = iter(compiler)
+
+    def read(self, n: int = -1) -> bytes:
+        bs = []
+
+        if n == -1:
+            try:
+                while True:
+                    bs.append(next(self.it))
+            except StopIteration:
+                pass
+        else:
+            try:
+                for _ in range(n):
+                    bs.append(next(self.it))
+            except StopIteration:
+                pass
+
+        return bytes(bs)
+
+
+def binary(compiler: Compiler) -> None:
+    shutil.copyfileobj(ReadCompiler(compiler), sys.stdout.buffer)
+
+
+def run(in_buf: TextIO, formatter: Formatter) -> None:
     try:
         compiler = SapCompiler.from_buf(in_buf)
     except Stop as e:
         raise Message(e.msg, e.toks)
 
-    for i, code in enumerate(compiler):
-        print(f"{i:04b}: {code >> 4:04b} {code & 15:04b}", file=out_buf)
+    formatter(compiler)
+
+
+FORMATTERS = {
+    "binary_text": binary_text,
+    "binary": binary,
+}
+
+parser = argparse.ArgumentParser(description="Compile an assembly file.")
+parser.add_argument(
+    "file",
+    nargs="?",
+    type=argparse.FileType("r"),
+    help="path to program file",
+    default=sys.stdin,
+)
+parser.add_argument(
+    "-f",
+    "--format",
+    help="format of compiled output",
+    default="binary_text",
+    choices=FORMATTERS.keys(),
+    required=False,
+)
 
 
 def main():  # pragma: no cover
-    if len(sys.argv) == 1:
-        file_txt = sys.stdin.read()
-        in_buf = StringIO(file_txt)
-    else:
-        if sys.argv[1] == "-h":
-            sys.stderr.write("usage: wes <filename> | wes < <filename>\n")
-            sys.exit(1)
+    args = parser.parse_args()
 
-        with open(sys.argv[1], "r") as f:
-            file_txt = f.read()
-        in_buf = StringIO(file_txt)
+    formatter = FORMATTERS[args.format]
+
+    file_txt = args.file.read()
+    in_buf = StringIO(file_txt)
 
     try:
-        run(in_buf, sys.stdout)
+        run(in_buf, formatter)
     except Message as e:
         print(e.render(file_txt), file=sys.stderr)
         sys.exit(1)
