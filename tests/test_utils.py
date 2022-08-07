b45dc754ae8f4e6675b780bab9965a7e4f2883b3
David Sanders <davesque@gmail.com>
2022-08-06 23:03:22 -0700

    Complete utils tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,4 +1,4 @@
-from wes.utils import str_to_int
+from wes.utils import byte_length, serialize_dict, str_to_int
 
 
 def test_str_to_int() -> None:
@@ -7,3 +7,16 @@ def test_str_to_int() -> None:
     assert str_to_int("0x2a") == 42
     assert str_to_int("42") == 42
     assert str_to_int("0042") == 42
+
+
+def test_byte_length() -> None:
+    assert byte_length(2**8 - 1) == 1
+    assert byte_length(2**8) == 2
+    assert byte_length(2**16 - 1) == 2
+    assert byte_length(2**16) == 3
+    assert byte_length(2**24 - 1) == 3
+    assert byte_length(2**24) == 4
+
+
+def test_serialize_dict() -> None:
+    assert serialize_dict({"b": 2, "a": 1}) == (("a", 1), ("b", 2))
