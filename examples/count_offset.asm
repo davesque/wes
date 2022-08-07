b9ff9d55faca62d02b3abf519b8f508938c7877d
David Sanders <davesque@gmail.com>
2022-08-06 23:45:01 -0700

    Add offset example

---
+++ examples/count_offset.asm
@@ -0,0 +1,24 @@
+; Counts from 42 to 256 (zero really in 8 bits), then down from 255 to 1
+; before halting
+
+lda init
+
+count_up:
+  out
+  add incr
+  jc count_down  ; jump to "count_down" if we overflowed
+  jmp count_up
+
+count_down:
+  out
+  sub incr
+  jz end         ; jump to "end" if we hit zero
+  jmp count_down
+
+end: hlt
+
+0    ; pad w/zeroes to variable memory
+-2:
+
+init: 42
+incr: 1
